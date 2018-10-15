52bbb3856d3c4c480017a6a6dd6a166374a75cfe
syoussef <Married=-09>
2018-10-15 09:02:21 -0400

    adding first chapter content

---
+++ Chapter 1/Code/ImportExample.java
@@ -0,0 +1,9 @@
+import java.util.*; // import tells us where to find Random
+
+public class ImportExample {
+  public static void main (String args[]){
+    Random r = new Random();
+    System.out.println(r.nextInt(10));
+
+  }
+}
